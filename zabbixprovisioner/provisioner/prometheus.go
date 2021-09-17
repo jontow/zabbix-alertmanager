62ad643b6552fdc61266f46125bcf16189e5f562
Jonathan Towne <jontow@unc.edu>
2021-09-17 12:37:16 -0400

    prov: add logging, support "alertname--severity"

--- zabbixprovisioner/provisioner/prometheus.go
+++ zabbixprovisioner/provisioner/prometheus.go
@@ -6,6 +6,7 @@ import (
 	"strings"
 
 	"github.com/pkg/errors"
+	log "github.com/sirupsen/logrus"
 	yaml "gopkg.in/yaml.v2"
 )
 
@@ -21,6 +22,7 @@ type PrometheusRule struct {
 	Expression  string            `yaml:"expr"`
 	Labels      map[string]string `yaml:"labels"`
 }
+
 
 func LoadPrometheusRulesFromDir(dir string) ([]PrometheusRule, error) {
 	filesInDir, err := ioutil.ReadDir(dir)
@@ -46,6 +48,13 @@ func LoadPrometheusRulesFromDir(dir string) ([]PrometheusRule, error) {
 			for _, rule := range ruleConfig.Groups {
 				for _, alert := range rule.Rules {
 					if alert.Name != "" {
+						if alert.Labels["severity"] != "" {
+							oldName := alert.Name
+							alert.Name = alert.Name + "--" + alert.Labels["severity"]
+						    log.Debugf("DEBUG: severity set, renaming %#v to %#v\n", oldName, alert.Name)
+						} else {
+							log.Debugf("DEBUG: severity NOT set, leaving name alone: %#v\n", alert.Name)
+						}
 						rules = append(rules, alert)
 					}
 				}
@@ -57,7 +66,12 @@ func LoadPrometheusRulesFromDir(dir string) ([]PrometheusRule, error) {
 	for i := 0; i < len(rules); i++ {
 		for j := i + 1; j < len(rules); j++ {
 			if rules[j].Name == rules[i].Name {
-				return nil, errors.Errorf("can't load rules with the same alertname: %v, index: %v, %v", rules[j].Name, i+1, j+1)
+				// One approach would be to fail because this is nonsense:
+				//return nil, errors.Errorf("can't load rules with the same alertname: %v, index: %v, %v", rules[j].Name, i+1, j+1)
+				// Let's take the way more expensive, irritating, insane option:
+				// ...reslice without one of these duplicate rules, then hope it all works out!
+				log.Debugf("DEBUG: removing duplicate copy of %#v (...to avoid explosions, of course)\n", rules[i].Name)
+				rules = append(rules[:i], rules[i+1:]...)
 			}
 		}
 	}
